b622859715a6043c597d5c0738a4de6d8d67ab3b
Dennis Colak <dennis.colak@pinecrest.edu>
2024-07-15 12:58:28 +0300

    added support for model in tkinter gui
    still printing to the console...

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+print("running")
+
 import os
 
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['TCL_LIBRARY'] = r'C:\Users\Dennis\AppData\Local\Programs\Python\Python312\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\Users\Dennis\AppData\Local\Programs\Python\Python312\tcl\tk8.6'
 
+import keras
 import tkinter as tk
 import numpy as np
+import threading
+import time
 
 W_WIDTH, W_HEIGHT = 1200, 600
 C_WIDTH, C_HEIGHT = 560, 560
-CIRCLE_RADIUS = 20
 
+# Initialize the grid and model globally
+grid = np.zeros((28, 28)).astype("float32")
+model = keras.models.load_model("models/nn.keras")
 
 def create_window():
     root = tk.Tk() # window creation
@@ -19,7 +27,7 @@ def create_window():
     return root
 
 
-def create_canvas():
+def create_canvas(root):
     # canvas creation
     canvas = tk.Canvas(root, width=C_WIDTH, height=C_HEIGHT, bg="black")
     canvas.place(x=28, rely=0.5, anchor="w")
@@ -40,15 +48,25 @@ def paint(event):
     canvas.create_rectangle(x*20, y*20, x*20+20, y*20+20, fill="white")
 
 
-if __name__ == "__main__":
-    # internal grid
-    grid = np.zeros((28, 28)).astype("float32")
+def ai_predict():
+    while True:
+        time.sleep(1)
+        # make a prediction using the model
+        pred = model.predict(np.array([grid]))
 
+        # print the prediction number + accuracy
+        print(f"Predicted Digit: {np.argmax(pred)} | Accuracy: {np.max(pred)*100:.2f}%")
+
+
+if __name__ == "__main__":
+    print("running")
     #TK GUI
     root = create_window()
 
-    canvas = create_canvas()
+    canvas = create_canvas(root)
     canvas.bind("<B1-Motion>", paint)  # event listener for mouse movement
 
+    ai_pred = threading.Thread(target=ai_predict, daemon=True)
+    ai_pred.start()
 
     root.mainloop()
